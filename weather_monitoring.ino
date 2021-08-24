827f1b040a8c9b7e6dc15b5fb1e46e6bbbdb701b
Hemant7777 <75162850+Hemant7777@users.noreply.github.com>
2021-08-24 17:22:18 +0530

    Add files via upload

---
+++ weather_monitoring.ino
@@ -0,0 +1,213 @@
+#include "DHT.h"
+#include "FS.h"
+#include "SD.h"
+#include <SPI.h>
+#include <WiFi.h>
+#include <NTPClient.h>
+#include <ArduinoJson.h>
+#include <WiFiUdp.h>
+#include <HTTPClient.h>
+
+#define DHTTYPE DHT11 // DHT 11
+#define uS_TO_S_FACTOR 1000000  //Conversion factor for micro seconds to seconds
+#define TIME_TO_SLEEP  20  
+uint8_t DHTPin = 4; 
+DHT dht(DHTPin, DHTTYPE); 
+float Temperature;
+float Humidity;
+String formattedDate;
+String dayStamp;
+String timeStamp;
+String deviceid="123";
+
+char server[] = "192.168.0.207";  
+
+
+WiFiClient client;    
+
+int    HTTP_PORT   = 80;
+RTC_DATA_ATTR int bootCount = 0;
+
+const char* ssid     = "NOVEL (ground floor)";         // The SSID (name) of the Wi-Fi network you want to connect toNOVEL (ground floor)
+const char* password ="Novel#123";
+//const char* ssid     = "NOVEL Java";         // The SSID (name) of the Wi-Fi network you want to connect toNOVEL (ground floor)
+//const char* password ="Novel#123";
+
+//const char* ssid     = "Hemant";// wifi name
+//const char* password = "12345678";// password
+// Define CS pin for the SD card module
+#define SD_CS 5
+String dataMessage;
+// Define NTP Client to get time
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
+void setup() {
+  Serial.begin(115200);
+  pinMode(DHTPin, INPUT);
+  dht.begin();
+  Serial.println("Hemant Weather report Task");
+  //Increment boot number and print it every reboot
+  ++bootCount;
+  Serial.println("Boot number: " + String(bootCount));
+
+  //Print the wakeup reason for ESP32
+  print_wakeup_reason();
+
+  //Set timer to 5 seconds
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+  Serial.println("Setup ESP32 to sleep for every " + String(TIME_TO_SLEEP) +
+  " Seconds");
+
+  
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected.");
+    Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+  // Initialize a NTPClient to get time
+  timeClient.begin();
+  timeClient.setTimeOffset(19800);
+  // Initialize SD card
+  SD.begin(SD_CS);  
+  if(!SD.begin(SD_CS)) {
+    Serial.println("Card Mount Failed");
+    return;
+  }
+  uint8_t cardType = SD.cardType();
+  if(cardType == CARD_NONE) {
+    Serial.println("No SD card attached");
+    return;
+  }
+  Serial.println("Initializing SD card...");
+  if (!SD.begin(SD_CS)) {
+    Serial.println("ERROR - SD card initialization failed!");
+    return;    // init failed
+  }
+  File file = SD.open("/Sensor.txt");
+  if(!file) {
+    Serial.println("File doens't exist");
+    Serial.println("Creating file...");
+    writeFile(SD, "/Sensor.txt", "DeviceId, Date, Time, Temperature, Humidity \r\n");
+  }
+  else {
+    Serial.println("File already exists");  
+  }
+  file.close();
+}
+void loop() {
+    Read_TempHum();
+    getTimeStamp();
+    logSDCard();
+   
+     postDataToServer();
+     esp_deep_sleep_start();
+  //  delay(5000); //Wait for 5 seconds before writing the next data 
+}
+// Function to get temperature
+void Read_TempHum()
+{
+  Temperature = dht.readTemperature(); 
+  Humidity = dht.readHumidity(); 
+  Serial.print("Temperature = ");
+  Serial.println(Temperature);
+  Serial.print("Humidity = ");
+  Serial.println(Humidity);
+}
+// Function to get date and time from NTPClient
+void getTimeStamp() {
+  while(!timeClient.update()) {
+    timeClient.forceUpdate();
+  }
+  formattedDate = timeClient.getFormattedDate();
+  Serial.println(formattedDate);
+  int splitT = formattedDate.indexOf("T");
+  dayStamp = formattedDate.substring(0, splitT);
+  Serial.println(dayStamp);
+  // Extract time
+  timeStamp = formattedDate.substring(splitT+1, formattedDate.length()-1);
+  Serial.println(timeStamp);
+}
+// Write the sensor readings on the SD card
+void logSDCard() {
+  dataMessage =  String(deviceid) + "," +String(dayStamp) + "," + String(timeStamp) + "," + 
+                String(Temperature) + "," + String(Humidity)+ "\r\n";
+  Serial.print("Save data: ");
+  Serial.println(dataMessage);
+  appendFile(SD, "/Sensor.txt", dataMessage.c_str());
+}
+// Write to the SD card (DON'T MODIFY THIS FUNCTION)
+void writeFile(fs::FS &fs, const char * path, const char * message) {
+  Serial.printf("Writing file: %s\n", path);
+  File file = fs.open(path, FILE_WRITE);
+  if(!file) {
+    Serial.println("Failed to open file for writing");
+    return;
+  }
+  if(file.print(message)) {
+    Serial.println("File written");
+  } else {
+    Serial.println("Write failed");
+  }
+  file.close();
+}
+// Append data to the SD card (DON'T MODIFY THIS FUNCTION)
+void appendFile(fs::FS &fs, const char * path, const char * message) {
+  Serial.printf("Appending to file: %s\n", path);
+  File file = fs.open(path, FILE_APPEND);
+  if(!file) {
+    Serial.println("Failed to open file for appending");
+    return;
+  }
+  if(file.print(message)) {
+    Serial.println("Message appended");
+  } else {
+    Serial.println("Append failed");
+  }
+  file.close();
+}
+
+void print_wakeup_reason(){
+  esp_sleep_wakeup_cause_t wakeup_reason;
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+  switch(wakeup_reason)
+  {
+    case 1  : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
+    case 2  : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
+    case 3  : Serial.println("Wakeup caused by timer"); break;
+    case 4  : Serial.println("Wakeup caused by touchpad"); break;
+    case 5  : Serial.println("Wakeup caused by ULP program"); break;
+    default : Serial.println("Wakeup was not caused by deep sleep"); break;
+  }
+}
+void postDataToServer() {
+     if (client.connect(server, 443)) {
+    Serial.println("connected");
+    // Make a HTTP request:
+    Serial.print("GET /My/iotdata.php?humidity=");
+    client.print("GET /My/iotdata.php?humidity=");     //http://localhost/My/iotdata.php?
+    Serial.println(Humidity);
+    client.print(Humidity);
+    client.print("&temperature=");
+    Serial.println("&temperature=");
+    client.print(Temperature);
+    Serial.println(Temperature);
+    client.print(" ");      //SPACE BEFORE HTTP/1.1
+    client.print("HTTP/1.1");
+    client.println();
+    client.println("Host: Your Local IP");
+    client.println("Connection: close");
+    client.println();
+  } else {
+    // if you didn't get a connection to the server:
+    Serial.println("connection failed");
+  }
+  
+}
+
+  
